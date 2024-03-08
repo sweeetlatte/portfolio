6ffca2ec7270021a05315df8b45cdeb02f222c46
Sweet Latte <thuylinh271055@gmail.com>
2024-03-08 18:51:17 +0700

    [feat] navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import './style.scss';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="left-section cormorant">
+        <a href="/">
+          <b>
+            Thuy <i>L</i>inh -
+          </b>{' '}
+          <i>A lorem ipsum developer</i>
+        </a>
+      </div>
+      <div className="right-section">
+        <a href="/">Home</a>
+        <a href="about">About</a>
+        <a href="work">Work</a>
+        <a href="contact">Contact</a>
+      </div>
+    </div>
+  );
+}
