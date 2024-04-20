0d1072ff20cff6c28a3ca24ed5198d2618ec756c
Sweet Latte <thuylinh271055@gmail.com>
2024-04-21 00:15:26 +0700

    [style + feat]: Home page - Move general styles to App.scss - Hover effect for "Let's talk" button - Banner in Work section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,15 @@
 import Icons from '../../components/Icons';
-import img from '../../assets/images/img_holder.jpg';
+import img from '../../assets/images/home_self.jpg';
 import './style.scss';
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="home__full-scr-flex-container gradient-bg_to-bottom">
+      <div className="full-scr-flex-container gradient-bg_to-bottom">
         <div className="home__hero-section">
-          <div className="home__big-title cormorant">I’M A FRONTEND DEVELOPER</div>
+          <div className="home__big-title cormorant">
+            I’M A <i>FRONTEND</i> DEVELOPER
+          </div>
           <div className="home__link-group">
             <a href="/" className="home__link-btn">
               Resumé <Icons.ArrowUpRight />
@@ -24,7 +26,7 @@ export default function Home() {
         </div>
         <div className="home__summary-content">
           <i className="home__summary-title cormorant">I AM THUY LINH.</i>
-          <p className="home__summary-text cormorant">
+          <p className="home__summary-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet,
             consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud exercitation
             ullamco laboris nisi ut aliquip ex ea commodo con Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
@@ -35,10 +37,31 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="home__full-scr-flex-container relative-container gradient-bg_to-top">
+      <div className="full-scr-flex-container relative-container gradient-bg_to-top">
         <div className="home__work-section">
           <div className="home__work-banner cormorant">
-            <i>K</i>IND <i>W</i>ORDS.
+            <div className="home__work-adjectives">
+              <span>
+                <i>O</i>pen-minded.&nbsp;
+              </span>
+              <span>
+                <i>C</i>urious.&nbsp;
+              </span>
+              <span>
+                <i>V</i>ersatile.&nbsp;
+              </span>
+            </div>
+            <div className="home__work-adjectives">
+              <span>
+                <i>O</i>pen-minded.&nbsp;
+              </span>
+              <span>
+                <i>C</i>urious.&nbsp;
+              </span>
+              <span>
+                <i>V</i>ersatile.&nbsp;
+              </span>
+            </div>
           </div>
           <div className="home__big-title cormorant">I HAVE WORKED LOREM IPSUM LOREM IPSUM</div>
           <a href="work" className="home__link-btn">
