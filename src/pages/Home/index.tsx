86c3c956b289f0b8fa07ca8d79139c74bfde8d71
Sweet Latte <thuylinh271055@gmail.com>
2024-04-28 23:46:22 +0700

    [chores] change from double quote to quote

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,46 +1,46 @@
 import Icons from '../../components/Icons';
-import img from '../../assets/images/home_self.jpg';
+import img from '../../assets/images/home_self_2.jpeg';
 import './style.scss';
 
 export default function Home() {
   return (
-    <div className="home">
-      <div className="full-scr-flex-container gradient-bg_to-bottom">
-        <div className="home__hero-section">
-          <div className="home__big-title cormorant">
+    <div className='home'>
+      <div className='full-scr-flex-container gradient-bg_to-bottom'>
+        <div className='home__hero-section'>
+          <div className='home__big-title cormorant'>
             I’M A <i>FRONTEND</i> DEVELOPER
           </div>
-          <div className="home__link-group">
-            <a href="/" className="home__link-btn">
+          <div className='home__link-group'>
+            <a href='/' className='home__link-btn'>
               Resumé <Icons.ArrowUpRight />
             </a>
-            <a href="work" className="home__link-btn">
+            <a href='works' className='home__link-btn'>
               Projects <Icons.ArrowUpRight />
             </a>
           </div>
         </div>
       </div>
-      <div className="home__summary-section">
-        <div className="home__summary-img">
-          <img src={img} alt="" />
+      <div className='home__summary-section'>
+        <div className='home__summary-img'>
+          <img src={img} alt='' />
         </div>
-        <div className="home__summary-content">
-          <i className="home__summary-title cormorant">I AM THUY LINH.</i>
-          <p className="home__summary-text">
+        <div className='home__summary-content'>
+          <i className='home__summary-title cormorant'>I AM THUY LINH.</i>
+          <p className='home__summary-text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet,
             consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud exercitation
             ullamco laboris nisi ut aliquip ex ea commodo con Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
             eu fugiat nulla
           </p>
-          <a href="/" className="home__link-btn">
+          <a href='/' className='home__link-btn'>
             More about me <Icons.ArrowUpRight />
           </a>
         </div>
       </div>
-      <div className="full-scr-flex-container relative-container gradient-bg_to-top">
-        <div className="home__work-section">
-          <div className="home__work-banner cormorant">
-            <div className="home__work-adjectives">
+      <div className='full-scr-flex-container relative-container gradient-bg_to-top'>
+        <div className='home__work-section'>
+          <div className='home__work-banner cormorant'>
+            <div className='home__work-adjectives'>
               <span>
                 <i>O</i>pen-minded.&nbsp;
               </span>
@@ -51,7 +51,7 @@ export default function Home() {
                 <i>V</i>ersatile.&nbsp;
               </span>
             </div>
-            <div className="home__work-adjectives">
+            <div className='home__work-adjectives'>
               <span>
                 <i>O</i>pen-minded.&nbsp;
               </span>
@@ -63,11 +63,11 @@ export default function Home() {
               </span>
             </div>
           </div>
-          <div className="home__big-title cormorant">I HAVE WORKED LOREM IPSUM LOREM IPSUM</div>
-          <a href="work" className="home__link-btn">
+          <div className='home__big-title cormorant'>I HAVE WORKED LOREM IPSUM LOREM IPSUM</div>
+          <a href='work' className='home__link-btn'>
             My projects <Icons.ArrowUpRight />
           </a>
-          <div className="home__talk-btn cormorant">LET’S TALK</div>
+          <div className='home__talk-btn cormorant'>LET’S TALK</div>
         </div>
       </div>
     </div>
